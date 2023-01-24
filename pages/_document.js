f94608461cbfd87f897a360913c6b823f7d7ef45
Nassim Kecira <92671036+nassimkcr@users.noreply.github.com>
2023-01-24 17:42:57 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet"/>
       </Head>
-      <body className="font-poppins">
+      <body>
         <Main />
         <NextScript />
       </body>
